d3087136d7655ad2450a1d9109caf5aae5563003
Gabriel Czajkowski <gabrielos123456789@gmail.com>
2021-07-20 11:51:46 +0200

    Returning Models Instead of Entities
    Returning Models Instead of Entities

--- API/Controllers/CampsController.cs
+++ API/Controllers/CampsController.cs
@@ -1,4 +1,6 @@
-﻿using CoreCodeCamp.Data;
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,20 +14,22 @@ namespace CoreCodeCamp.Controllers
     public class CampsController : ControllerBase
     {
         private readonly ICampRepository campRepository;
+        private readonly IMapper mapper;
 
-        public CampsController(ICampRepository campRepository)
+        public CampsController(ICampRepository campRepository, IMapper mapper)
         {
             this.campRepository = campRepository;
+            this.mapper = mapper;
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<ActionResult<CampModel[]>> Get()
         {
             try
             {
                 var result = await campRepository.GetAllCampsAsync();
 
-                return Ok(result);
+                return mapper.Map<CampModel[]>(result);
             }
             catch (Exception)
             {
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using CoreCodeCamp.Data;
 using Microsoft.AspNetCore.Builder;
@@ -20,6 +21,8 @@ namespace CoreCodeCamp
         {
             services.AddDbContext<CampContext>();
             services.AddScoped<ICampRepository, CampRepository>();
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
 
             services.AddControllers();
         }
