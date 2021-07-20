8225ecd39bdef07daf8d96ddb6f0df0d37f21565
Gabriel Czajkowski <gabrielos123456789@gmail.com>
2021-07-20 10:55:05 +0200

    Creating an Action
    Creating an Action

---
+++ API/Controllers/CampsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : ControllerBase
+    {
+        public object Get()
+        {
+            return new
+            {
+                Moniker = "ATL2018",
+                Name = "Atlatna Code Camp"
+            };
+        }
+    }
+}
