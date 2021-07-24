b5697eeac0ff09f3260ac0f8acdafc44665235c3
Gabriel Czajkowski <gabrielos123456789@gmail.com>
2021-07-24 19:57:39 +0200

    Add Validation
    Add Validation

--- API/Models/TalkModel.cs
+++ API/Models/TalkModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,10 +22,15 @@ namespace CoreCodeCamp.Models
 
         public int TalkId { get; set; }
 
+        [Required]
+        [StringLength(100)]
         public string Title { get; set; }
 
+        [Required]
+        [StringLength(4000, MinimumLength = 20)]
         public string Abstract { get; set; }
 
+        [Range(100, 300)]
         public int Level { get; set; }
 
         public SpeakerModel Speaker { get; set; }
