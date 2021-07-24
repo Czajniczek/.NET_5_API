b19940fb83f1fb4fcec5f6203818ac7a68e7e258
Gabriel Czajkowski <gabrielos123456789@gmail.com>
2021-07-24 21:36:48 +0200

    Creating a Functional API
    Creating a Functional API

---
+++ API/Controllers/OperationsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OperationsController : ControllerBase
+    {
+        private readonly IConfiguration config;
+
+        public OperationsController(IConfiguration config)
+        {
+            this.config = config;
+        }
+
+        [HttpOptions("reloadconfig")]
+        public IActionResult ReloadConfig()
+        {
+            try
+            {
+                var root = (IConfigurationRoot)config;
+                root.Reload();
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
