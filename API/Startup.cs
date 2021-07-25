784fa43ea6eb030bbf118ea6d6b476bbd398abfa
Gabriel Czajkowski <gabrielos123456789@gmail.com>
2021-07-26 01:51:33 +0200

    Versioning Conventions
    Versioning Conventions

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using CoreCodeCamp.Controllers;
 using CoreCodeCamp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -13,6 +14,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Mvc.Versioning.Conventions;
 
 namespace CoreCodeCamp
 {
@@ -34,8 +36,16 @@ namespace CoreCodeCamp
                 //opt.ApiVersionReader = ApiVersionReader.Combine(
                 //    new QueryStringApiVersionReader("ver", "version"),
                 //    new HeaderApiVersionReader("X-Version"));
+
                 // URL Versioning
                 opt.ApiVersionReader = new UrlSegmentApiVersionReader();
+
+                // Versioning Conventions
+                //opt.Conventions.Controller<TalksController>()
+                //.HasApiVersion(new ApiVersion(1, 0))
+                //.HasApiVersion(new ApiVersion(1, 1))
+                //.Action(c => c.Delete(default(string), default(int)))
+                //.MapToApiVersion(1, 1);
             });
 
             //services.AddControllers();
