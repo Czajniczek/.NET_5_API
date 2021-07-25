614b34d00d70f7b71a24c194aade4659de02a2ff
Gabriel Czajkowski <gabrielos123456789@gmail.com>
2021-07-26 01:23:06 +0200

    Versioning with Headers
    Versioning with Headers

--- API/Startup.cs
+++ API/Startup.cs
@@ -7,6 +7,7 @@ using CoreCodeCamp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,6 +31,8 @@ namespace CoreCodeCamp
                 opt.AssumeDefaultVersionWhenUnspecified = true;
                 opt.DefaultApiVersion = new ApiVersion(1, 1);
                 opt.ReportApiVersions = true;
+                //opt.ApiVersionReader = new QueryStringApiVersionReader("ver");
+                opt.ApiVersionReader = new HeaderApiVersionReader("X-Version");
             });
 
             //services.AddControllers();
