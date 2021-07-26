a7d9a020f80be972e24819c56cd914ded70ca7fd
Gabriel Czajkowski <gabrielos123456789@gmail.com>
2021-07-26 21:17:52 +0200

    Final API
    Final API

--- API/Data/Entities/Talk.cs
+++ API/Data/Entities/Talk.cs
@@ -3,15 +3,15 @@
     public class Talk
     {
         public int TalkId { get; set; }
-        
+
         public Camp Camp { get; set; }
-        
+
         public string Title { get; set; }
-        
+
         public string Abstract { get; set; }
-        
+
         public int Level { get; set; }
-        
+
         public Speaker Speaker { get; set; }
     }
 }
